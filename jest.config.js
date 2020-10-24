fc7655ef60724e0daa1cd2275791e1e121e11466
Matthew Finucane <matfin@gmail.com>
2020-10-24 20:18:18 +0100

    feat: project kick off

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  globals: {
+    window: true
+  },
+  rootDir: '.',
+  coverageDirectory: './coverage',
+  setupFiles: ['./jestsetup.js'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'testutils.ts',
+  ],
+  moduleDirectories: ['node_modules'],
+  moduleNameMapper: {
+    '^assets/(.*)$': '<rootDir>/assets/$1',
+    '^components/(.*)$': '<rootDir>/src/components/$1',
+    '^hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^lib/(.*)$': '<rootDir>/src/lib/$1',
+    '^models/(.*)$': '<rootDir>/src/models/$1',
+    '^pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^(config|styles|testutils|utils)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom-sixteen'
+};
